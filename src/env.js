2fb19d608f04ba0896a29bc201dce6a71a35ed32
Adeel <adeel.abass15@gmail.com>
2023-05-25 22:21:36 +0500

    first commit

---
+++ src/env.js
@@ -0,0 +1,7 @@
+export default
+    {
+        "API_URL": "https://localhost", // Address for backend api url
+        "PORT": 44300, // port of backend api 
+        "SOCKET": "http://127.0.0.1:4001", // socket url
+        "JITSI": "meet.jit.si" // jitsi cloud server url
+    }
